424917a24834183f8cdac2ea984478a39aa08905
Sean Pierce Sumler <sumler.sean@gmail.com>
2019-04-15 16:26:55 -0700

    Scaffold navigation for admin index view

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -5,9 +5,19 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Posts</div>
+                <div class="card-header">Welcome Admin!</div>
                 <div class="card-body">
-                    <a href="admin/post">Create Post</a>
+                    <ul class="list-group">
+                        <a href="/admin/posts" class="list-group-item list-group-item-action">
+                            Posts
+                        </a>
+                        <a href="#" class="list-group-item list-group-item-action">
+                            Lorem Ipsum
+                        </a>
+                        <a href="#" class="list-group-item list-group-item-action">
+                            Some New Link
+                        </a>
+                    </ul>
                 </div>
             </div>
         </div>
