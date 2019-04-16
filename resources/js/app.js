b1353aae322b820472f776071be0435e94e6fb07
Sean Pierce Sumler <sumler.sean@gmail.com>
2019-04-15 21:06:33 -0700

    Create and import new toaster component

--- resources/js/app.js
+++ resources/js/app.js
@@ -20,7 +20,8 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i);
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
-Vue.component('create-post', require('./components/CreatePost.vue').default);
+Vue.component('toaster', require('./components/Shared/Toaster.vue').default);
+Vue.component('create-post', require('./components/Admin/CreatePost.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
---
+++ resources/js/components/Shared/toaster.vue
@@ -0,0 +1,63 @@
+<template>
+    <div>
+        <div
+            v-if="$root.success.popped"
+            @click="hideToaster('success')"
+            class="pop-success">
+            {{ $root.success.message }}
+        </div>
+        <div
+            v-if="$root.error.popped"
+            @click="hideToaster('error')"
+            class="pop-error">
+            {{ $root.error.message }}
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+        },
+        methods: {
+            hideToaster(type) {
+                switch (type) {
+                    case "success":
+                        this.$root.clearSuccess();
+                        break;
+                    case "error":
+                        this.$root.clearError();
+                        break;;
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+.pop-success,
+.pop-error {
+    width: 90%;
+    max-width: 300px;
+    padding: 2em;
+    color: white;
+    opacity: 0.8;
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+    border-radius: 2px;
+    font-size: 1.25em;
+    z-index: 10;
+    cursor: pointer;
+}
+
+.pop-success {
+    background: #28a745;
+}
+
+.pop-error {
+    background: #dc3545;
+}
+</style>
